59e14bae0614656f744131d871c0e9c9bfce5b3d
Shubham Patgavkar <shubhampatgavkar5@gmail.com>
2025-07-14 20:18:39 +0530

    lambda Expression added

--- Interfaces.java
+++ Interfaces.java
@@ -11,5 +11,14 @@ public class Interfaces {
     public static void main(String args[]){
         A a1 = new A();
         a1.show();
+
+
+        // rather than implementing the interface in the class we can use the lamda expression to implement the show method
+        // we don't need to specify the name of the method since the functional interface has only one abstract method
+        myInterface b1 = () -> System.out.println("This is Lamda function Implementation");
+        // this technique avoid to implement the interface in the class
+        b1.show();
     }
+
+
 }
